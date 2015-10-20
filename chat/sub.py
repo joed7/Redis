d4527135c8f010ab2c67593fe7b2e7a9c2a10318
rohit <rohit@work.com>
2015-10-19 22:52:12 -0600

    remove subscribe messages

--- chat/sub.py
+++ chat/sub.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     channel = sys.argv[1]
     r = redis.Redis(host='localhost',port=6379)        
 
-    pubsub = r.pubsub()
+    pubsub = r.pubsub(ignore_subscribe_messages=True)
     pubsub.subscribe(channel)
 
     print 'Listening to '+channel
